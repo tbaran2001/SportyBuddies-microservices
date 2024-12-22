2fe6ca2f61636aea788dd8f778211e942d430095
tbaran2001 <barantomasz2001@gmail.com>
2024-12-22 22:49:14 +0100

    Add Building blocks messaging project

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/ProfileSportAddedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record ProfileSportAddedEvent : IntegrationEvent
+{
+    public Guid ProfileId { get; init; }
+    public Guid SportId { get; init; }
+}
