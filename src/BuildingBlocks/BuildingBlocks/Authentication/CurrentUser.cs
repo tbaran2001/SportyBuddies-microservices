340350a8b6ce41d68ac1bf41e9b604de31dff372
tbaran2001 <barantomasz2001@gmail.com>
2024-12-28 22:05:06 +0100

    Add user context to building blocks

---
+++ src/BuildingBlocks/BuildingBlocks/Authentication/CurrentUser.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks.Authentication;
+
+public record CurrentUser(Guid Id, IEnumerable<string> Roles)
+{
+    public bool IsInRole(string role) => Roles.Contains(role);
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Authentication/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Authentication;
+
+public class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public CurrentUser GetCurrentUser()
+    {
+        var user = httpContextAccessor.HttpContext?.User;
+        if (user == null)
+            throw new InvalidOperationException("User Context is not available");
+
+        if (user.Identity == null || !user.Identity.IsAuthenticated)
+            throw new Exception("User is not authenticated");
+
+        var userId = user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+        var roles = user.FindAll(ClaimTypes.Role).Select(c => c.Value);
+
+        return new CurrentUser(Guid.Parse(userId),roles);
+    }
+}
