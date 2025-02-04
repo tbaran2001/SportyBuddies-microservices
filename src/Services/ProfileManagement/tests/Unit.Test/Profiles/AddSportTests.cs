dfece6d578dd3a05b38f94c5fbede7c0b3a52c71
tbaran2001 <barantomasz2001@gmail.com>
2025-02-04 22:58:05 +0100

    Tests for AddSport and RemoveSport

---
+++ src/Services/ProfileManagement/tests/Unit.Test/Profiles/AddSportTests.cs
@@ -0,0 +1,52 @@
+﻿using BuildingBlocks.Exceptions;
+using FluentAssertions;
+using ProfileManagement.API.Profiles.Features.Commands.AddProfileSport;
+using ProfileManagement.API.Profiles.Models;
+using ProfileManagement.API.Profiles.ValueObjects;
+
+namespace Unit.Test.Profiles;
+
+public class AddSportTests
+{
+    [Fact]
+    public void AddSport_WhenUserDoesNotHaveSport_ShouldAddSport()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(),ProfileName.Create("User"), ProfileDescription.Create("Description"));
+
+        // Act
+        user.AddSport(Guid.NewGuid());
+
+        // Assert
+        user.ProfileSports.Should().ContainSingle();
+    }
+
+    [Fact]
+    public void AddSport_WhenUserHasSport_ShouldThrowException()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(),ProfileName.Create("User"), ProfileDescription.Create("Description"));
+        var sportId = Guid.NewGuid();
+        user.AddSport(sportId);
+
+        // Act
+        var act = () => user.AddSport(sportId);
+
+        // Assert
+        act.Should().Throw<DomainException>();
+    }
+
+    [Fact]
+    public void AddSport_Should_RaiseSportAddedDomainEvent()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(),ProfileName.Create("User"), ProfileDescription.Create("Description"));
+        var sportId = Guid.NewGuid();
+
+        // Act
+        user.AddSport(sportId);
+
+        // Assert
+        user.DomainEvents.Should().ContainSingle(e => e is ProfileSportAddedDomainEvent);
+    }
+}
---
+++ src/Services/ProfileManagement/tests/Unit.Test/Profiles/RemoveSportTests.cs
@@ -0,0 +1,54 @@
+﻿using BuildingBlocks.Exceptions;
+using FluentAssertions;
+using ProfileManagement.API.Profiles.Features.Commands.RemoveProfileSport;
+using ProfileManagement.API.Profiles.Models;
+using ProfileManagement.API.Profiles.ValueObjects;
+
+namespace Unit.Test.Profiles;
+
+public class RemoveSportTests
+{
+    [Fact]
+    public void RemoveSport_WhenUserHasSport_ShouldRemoveSport()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(), ProfileName.Create("User"), ProfileDescription.Create("Description"));
+        var sportId = Guid.NewGuid();
+        user.AddSport(sportId);
+
+        // Act
+        user.RemoveSport(sportId);
+
+        // Assert
+        user.ProfileSports.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RemoveSport_WhenUserDoesNotHaveSport_ShouldThrowException()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(), ProfileName.Create("User"), ProfileDescription.Create("Description"));
+        var sportId = Guid.NewGuid();
+
+        // Act
+        var act = () => user.RemoveSport(sportId);
+
+        // Assert
+        act.Should().Throw<DomainException>();
+    }
+
+    [Fact]
+    public void RemoveSport_Should_RaiseSportRemovedDomainEvent()
+    {
+        // Arrange
+        var user = Profile.CreateSimple(Guid.NewGuid(), ProfileName.Create("User"), ProfileDescription.Create("Description"));
+        var sportId = Guid.NewGuid();
+        user.AddSport(sportId);
+
+        // Act
+        user.RemoveSport(sportId);
+
+        // Assert
+        user.DomainEvents.Should().ContainSingle(e => e is ProfileSportRemovedDomainEvent);
+    }
+}
