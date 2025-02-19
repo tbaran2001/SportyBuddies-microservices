225873f57d7671d64e1cd639f8fbab8984e73264
tbaran2001 <barantomasz2001@gmail.com>
2025-02-19 18:43:29 +0100

    Add serilog, elasticsearch and kibana, test logging with profilemanagement

--- src/Services/ProfileManagement/src/ProfileManagement.API/Program.cs
+++ src/Services/ProfileManagement/src/ProfileManagement.API/Program.cs
@@ -1,6 +1,27 @@
 using ProfileManagement.API.Extensions;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+{
+    loggerConfig.Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(
+            new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
+            {
+                IndexFormat =
+                    $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                NumberOfShards = 2,
+                NumberOfReplicas = 1
+            })
+        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+        .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+        .ReadFrom.Configuration(context.Configuration);
+});
 
 builder.AddInfrastructure();
 
