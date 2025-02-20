4a248c819a59b48b3a943a6dbf074394a19ba5a7
tbaran2001 <barantomasz2001@gmail.com>
2025-02-20 20:22:24 +0100

    Add mapster config for sport microservice

---
+++ src/Services/Sport/Sport.API/Sports/Mapster/MapsterConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Sport.API.Sports.Mapster;
+
+public class MapsterConfig
+{
+    public static void Configure()
+    {
+        TypeAdapterConfig<Name, string>.NewConfig()
+            .MapWith(src => src.Value);
+
+        TypeAdapterConfig<Description, string>.NewConfig()
+            .MapWith(src => src.Value);
+    }
+}
