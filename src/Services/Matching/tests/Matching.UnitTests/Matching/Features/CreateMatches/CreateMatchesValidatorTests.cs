894f53213fb2f85133a151d69774bebb64dfdff8
tbaran2001 <barantomasz2001@gmail.com>
2025-03-04 18:20:58 +0100

    Add unit tests for CreateMatches and UpdateMatch command validation

---
+++ src/Services/Matching/tests/Matching.UnitTests/Matching/Features/CreateMatches/CreateMatchesValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+
+namespace Matching.UnitTests.Matching.Features.CreateMatches;
+
+public class CreateMatchesValidatorTests : ValidatorTestBase<CreateMatchesCommand>
+{
+    protected override CreateMatchesCommand CreateValidObject()
+    {
+        return new FakeCreateMatchesCommand().Generate();
+    }
+
+    protected override IValidator<CreateMatchesCommand> CreateValidator()
+    {
+        return new CreateMatchesCommandValidator();
+    }
+
+    [Fact]
+    protected override void Validate_ShouldPass_WhenAllPropertiesAreValid()
+    {
+        base.Validate_ShouldPass_WhenAllPropertiesAreValid();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenProfileIdIsEmpty()
+    {
+        // Act
+        var result = Validate(x => x with { ProfileId = Guid.Empty });
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ProfileId);
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenMatchedProfileIdIsEmpty()
+    {
+        // Act
+        var result = Validate(x => x with { MatchedProfileId = Guid.Empty });
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.MatchedProfileId);
+    }
+}
---
+++ src/Services/Matching/tests/Matching.UnitTests/Matching/Features/UpdateMatch/UpdateMatchValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+
+namespace Matching.UnitTests.Matching.Features.UpdateMatch;
+
+public class UpdateMatchValidatorTests : ValidatorTestBase<UpdateMatchCommand>
+{
+    protected override UpdateMatchCommand CreateValidObject()
+    {
+        return new UpdateMatchCommand(Guid.NewGuid(), Swipe.Right);
+    }
+
+    protected override IValidator<UpdateMatchCommand> CreateValidator()
+    {
+        return new UpdateMatchCommandValidator();
+    }
+
+    [Fact]
+    protected override void Validate_ShouldPass_WhenAllPropertiesAreValid()
+    {
+        base.Validate_ShouldPass_WhenAllPropertiesAreValid();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenMatchIdIsEmpty()
+    {
+        // Act
+        var result = Validate(x => x with { MatchId = Guid.Empty });
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.MatchId);
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenSwipeIsInvalid()
+    {
+        // Act
+        var result = Validate(x => x with { Swipe = (Swipe)100 });
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Swipe);
+    }
+}
