6730ef6a9251d79eb1521af0025a1664bdc99603
tbaran2001 <barantomasz2001@gmail.com>
2025-03-05 23:52:43 +0100

    Add integration tests for RemoveProfileSport command and update global usings

---
+++ src/Services/ProfileManagement/tests/ProfileManagement.IntegrationTests/Profiles/Features/Commands/RemoveProfileSportTests.cs
@@ -0,0 +1,47 @@
+﻿namespace ProfileManagement.IntegrationTests.Profiles.Features.Commands;
+
+public class RemoveProfileSportTests(IntegrationTestWebAppFactory factory) : ProfileServiceIntegrationTestBase(factory)
+{
+    [Fact]
+    public async Task RemoveProfileSport_ShouldRemoveProfileSport_WhenProfileHasSport()
+    {
+        // Arrange
+        var fakeProfile = new FakeProfile().Generate();
+        var fakeSport = new FakeSport().Generate();
+
+        fakeProfile.AddSport(fakeSport.Id);
+
+        DbContext.Sports.Add(fakeSport);
+        DbContext.Profiles.Add(fakeProfile);
+        await DbContext.SaveChangesAsync();
+
+        var command = new RemoveProfileSportCommand(fakeProfile.Id.Value, fakeSport.Id.Value);
+
+        // Act
+        var result = await Sender.Send(command);
+
+        // Assert
+        result.Should().Be(Unit.Value);
+        fakeProfile.ProfileSports.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task RemoveProfileSport_ShouldThrowDomainException_WhenProfileDoesNotHaveSport()
+    {
+        // Arrange
+        var fakeProfile = new FakeProfile().Generate();
+        var fakeSport = new FakeSport().Generate();
+
+        DbContext.Sports.Add(fakeSport);
+        DbContext.Profiles.Add(fakeProfile);
+        await DbContext.SaveChangesAsync();
+
+        var command = new RemoveProfileSportCommand(fakeProfile.Id.Value, fakeSport.Id.Value);
+
+        // Act
+        Func<Task> act = async () => { await Sender.Send(command); };
+
+        // Assert
+        await act.Should().ThrowAsync<DomainException>();
+    }
+}
